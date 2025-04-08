17f0678315de8d19b6d62096eac1285dd6936d0b
LeandroBona <leandro_bona@outlook.com>
2025-04-08 07:35:29 -0300

    ultimas astualizções

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,39 @@
+const personagens = document.querySelectorAll('.personagem')
+
+personagens.forEach((personagem) => {
+    personagem.addEventListener('mouseenter', () => {
+        if(window.innerHTML < 450){
+            window.scrollTo({top: 0, behavior: 'smooth'})
+        }
+
+
+        
+        removerSelecaoPersonagem()
+        personagem.classList.add('selecionado')
+        alterarDescricaoPersonagem(personagem)
+        alterarNomePersonagemSelecionado(personagem)
+        alterarImagemPersonagemSelecionado(personagem)
+    })
+})
+
 function alterarDescricaoPersonagem(personagem){
     const descricaoPersonagem = document.getElementById('descricao-personagem')
     descricaoPersonagem.innerHTML = personagem.getAttribute('data-description')
 }
 
-function alterarNomePersonagemSelcionado(personagem){
+function alterarNomePersonagemSelecionado(personagem){
     const nomePersonagem = document.getElementById('nome-personagem') 
-    nomePersonagem = personagem.getAttribute('data-name')
+    nomePersonagem.textContent = personagem.getAttribute('data-name')
+}
+
+function alterarImagemPersonagemSelecionado(personagem){
+    const imagemPersonagemGrande = document.querySelector('.personagem-grande')
+    const idPersonagem = personagem.getAttribute('id')
+    imagemPersonagemGrande.src = `./assets/images/card-${idPersonagem}.png`
+    
+}
+
+function removerSelecaoPersonagem(){
+    const personagemSelecionado = document.querySelector('.selecionado')
+    personagemSelecionado.classList.remove('selecionado')
 }
